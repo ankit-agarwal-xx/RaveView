361dbe0faef7c0d19e133334d5096b197bc8747f
AnkitxxChici <ankuuuuuu23@gmail.com>
2023-09-16 21:08:12 +0530

    api(s) added

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <script setup>
+const movies = ref([]);
+
+// Search Movie
 const searchError = ref(false);
-const searchMovie= (searchInput) =>{
+const searchMovie= async (searchInput) =>{
     //console.log("Search Text :", searchInput)
     if(searchInput === ""){
         searchError.value = true;
@@ -8,25 +11,27 @@ const searchMovie= (searchInput) =>{
     if(searchInput !== ""){
         searchError.value = false;
         //Movie search code
+        const { data, error } = await useFetch(
+      `/api/movies/search?searchInput=${searchInput}`
+    );
+    movies.value = data.value;
     }
 };
-// Fetch Movie
-const movieid = ref(123)
-const title = ref(" The Batman")
-const date = ref("20-01-2023")
-const poster= ref("https://www.themoviedb.org/t/p/w600_and_h900_bestv2/74xTEgt7R36Fpooo50r9T25onhq.jpg")
+// Fetch All Movies
+const   {data , error} = await useFetch("/api/movies")
+movies.value = data.value;
 </script>
 
 <template>
     <div>
-       <SearchBar @search-movie="searchMovieearchMovie" :searchError="searchError"/>
+       <SearchBar @search-movie="searchMovie" :searchError="searchError"/>
     <div class="grid md:grid-cols-4 sm:grid-cols-2 justify-items-center bg-stone-800">
-    <div v-for="n in 8">
+    <div v-for="movie in movies.results" :key="movie.id">
     <MovieCard 
-    :title="title" 
-    :date="date" 
-    :poster="poster" 
-    :movieid="movieid"
+    :title="movie.title" 
+    :date="movie.release_date" 
+    :poster="movie.poster_path" 
+    :movieid="movie.id"
     />
     </div>
     </div>
---
+++ server/api/movies/[id].js
@@ -0,0 +1,11 @@
+export default defineEventHandler(async (event) => {
+    const { movieKey } = useRuntimeConfig()
+    const movieid = event.context.params.id
+    try {
+      const data = await $fetch(`https://api.themoviedb.org/3/movie/${movieid}?api_key=${movieKey}&language=en-US`)
+      return data
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  })
